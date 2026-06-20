e660effcf204c91842808beb4afbe19a0bc4dca0
shamir <shamir.mantilla@unmsm.edu.pe>
2026-06-20 06:30:46 -0500

    feat: Update Dockerfile and settings for production deployment with Gunicorn and Whitenoise

--- backend/src/config/settings.py
+++ backend/src/config/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -88,16 +89,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": env("POSTGRES_DB", default="bladder_db"),
-        "USER": env("POSTGRES_USER", default="bladder_user"),
-        "PASSWORD": env("POSTGRES_PASSWORD", default="bladder_password"),
-        "HOST": env("POSTGRES_HOST", default="db"),
-        "PORT": env("POSTGRES_PORT", default="5432"),
-    }
-}
+_default_db = "postgres://{}:{}@{}:{}/{}".format(
+    env("POSTGRES_USER", default="bladder_user"),
+    env("POSTGRES_PASSWORD", default="bladder_password"),
+    env("POSTGRES_HOST", default="db"),
+    env("POSTGRES_PORT", default="5432"),
+    env("POSTGRES_DB", default="bladder_db"),
+)
+DATABASES = {"default": env.db("DATABASE_URL", default=_default_db)}
 
 
 # Password validation
@@ -135,6 +134,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': env('CLOUDINARY_CLOUD_NAME'),
